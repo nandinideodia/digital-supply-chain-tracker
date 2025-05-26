64cacd0fe970c4abead63bc54e8457847dee9eca
SpandanDS <ss6636@srmist.edu.in>
2025-05-26 18:08:48 +0530

    [Spandan] User attributers and enums package

--- src/main/java/com/example/supplytracker/entity/User.java
+++ src/main/java/com/example/supplytracker/entity/User.java
@@ -1,5 +1,28 @@
 package com.example.supplytracker.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+import com.example.supplytracker.enums.Role;
 
+@Entity
+@Data
+@Table(name="USERS")
+@NoArgsConstructor // to create constructor with no parameters
+@AllArgsConstructor // to create constructor with the below parameters
+
+public class User
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id; //@Id and @GeneratedValue will help me generate a primary key id
+	
+	private String name;
+	
+	@Column(unique=true) // it causes the email to be unique for each user
+	private String email; 
+
+	private String password;
+	
+	@Enumerated(EnumType.STRING) // maps the roles present in enums to this as a string 
+		private Role role;
 }
---
+++ src/main/java/com/example/supplytracker/enums/Role.java
@@ -0,0 +1,9 @@
+package com.example.supplytracker.enums;
+
+public enum Role // below are the roles as described 
+{
+	Admin,
+	Supplier,
+	Transporter,
+	WarehouseMgr
+}

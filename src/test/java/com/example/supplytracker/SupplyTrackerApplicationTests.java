14142f0802c53b97361dcc670223a8686a761bef
Nandini Deodia <nandinideodia.2406@gmail.com>
2025-05-26 10:01:05 +0530

    initial commit

---
+++ src/test/java/com/example/supplytracker/SupplyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supplytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

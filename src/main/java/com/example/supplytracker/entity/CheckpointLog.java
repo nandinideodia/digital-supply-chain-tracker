d0b8931d468a6f125d5f28152d371a8f58425178
Satya Koushik <satyakoushik6@gmail.com>
2025-05-27 12:51:00 +0530

    [koushik]added checkpointlog

--- src/main/java/com/example/supplytracker/entity/CheckpointLog.java
+++ src/main/java/com/example/supplytracker/entity/CheckpointLog.java
@@ -1,5 +1,28 @@
 package com.example.supplytracker.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CheckpointLog {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String location;
+
+    private String status; 
+
+    private LocalDateTime timestamp;
+
+    @ManyToOne
+    @JoinColumn(name = "shipment_id")
+    private Shipment shipment;
 }
+

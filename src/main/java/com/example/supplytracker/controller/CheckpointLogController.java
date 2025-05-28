bde6ec885b35ad9ed534f953cb0a7a0acdb1f4c0
SaiVarun107 <saivarunbitra107.com>
2025-05-28 17:14:18 +0530

    [Varun] Checkpoints module Added

---
+++ src/main/java/com/example/supplytracker/controller/CheckpointLogController.java
@@ -0,0 +1,43 @@
+package com.example.supplytracker.controller;
+
+import com.example.supplytracker.dto.CheckpointLogDTO;
+import com.example.supplytracker.service.CheckpointLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/checkpoints")
+@CrossOrigin
+public class CheckpointLogController {
+
+    @Autowired
+    private CheckpointLogService checkpointLogService;
+
+    @GetMapping
+    public List<CheckpointLogDTO> getAllLogs() {
+        return checkpointLogService.getAllLogs();
+    }
+
+    @GetMapping("/{id}")
+    public CheckpointLogDTO getLogById(@PathVariable Long id) {
+        return checkpointLogService.getLogById(id);
+    }
+
+    @PostMapping
+    public CheckpointLogDTO addLog(@RequestBody CheckpointLogDTO checkpointLog) {
+        return checkpointLogService.addLog(checkpointLog);
+    }
+
+    @PutMapping("/{id}")
+    public CheckpointLogDTO updateLog(@PathVariable Long id, @RequestBody CheckpointLogDTO updatedLog) {
+        return checkpointLogService.updateLog(id, updatedLog);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteLog(@PathVariable Long id) {
+        checkpointLogService.deleteLogById(id);
+        return "Log deleted";
+    }
+}
